9fbaadd93c1224711faf48466bb6de44c905633a
racxak <92050973+racxak@users.noreply.github.com>
2024-02-04 14:17:53 +0100

    contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import About from './components/pages/About';
+import {Contact} from './components/pages/Contact/Contact'
 import "./App.css"
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   <Routes>
     <Route path="/about" element={<About/>}></Route>
     <Route path="/projects" element={<About/>}></Route>
-    <Route path="/contact" element={<About/>}></Route>
+    <Route path="/contact" element={<Contact/>}></Route>
     <Route path="/cart" element={<About/>}></Route>
 
   </Routes>
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,4 @@
 export default function About(){
-  return(<>
-  
-
-  
-  </>);
+  return(<div>
+  </div>);
 };
