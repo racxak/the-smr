419a64cb531b2609958f66e3a8135534e0932f94
racxak <92050973+racxak@users.noreply.github.com>
2024-03-08 12:18:53 +0100

    Login&SignUp

--- src/components/pages/Auth/Login.jsx
+++ src/components/pages/Auth/Login.jsx
@@ -5,14 +5,11 @@ import usePasswordToogle from "../../../hooks/usePasswordToogle";
 
 export default function Login ({changeForm}) {
   const [InputType, Icon] = usePasswordToogle();
-
+  
 	return (
 		<div>
 			<form className="form">
 				<h1>Sign in </h1>
-				<label> Full Name </label>
-        <input type="text" required placeholder="Jhon Doe"
-				></input>
 
 				<label> Email </label>
 				<input type="email" required placeholder="jhon@email.com"
@@ -29,7 +26,6 @@ export default function Login ({changeForm}) {
       <div className="form-buttons-layout">
       <span>You dont have an account yet? <button onClick={()=>changeForm("register")}> Click here to register </button></span>
       <button type="submit">
-
 					Sign in
 				</button>
      </div>
--- src/components/pages/Auth/SignUp.jsx
+++ src/components/pages/Auth/SignUp.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import usePasswordToogle from "../../../hooks/usePasswordToogle";
 
 export default function SignUp ({changeForm}) {
   const [InputType, Icon] = usePasswordToogle();
 
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessrMsg] = useState("");
+  
+  function handleSignUp(e){
+    e.preventDefault();
+    console.log(fullName, email, password)
+  }
+
 	return (
     <div>
-    <form className="form">
+    <form className="form" onSubmit={handleSignUp}>
       <h1>Sign up </h1>
 
+      <label> Full Name </label>
+        <input type="text" required placeholder="Jhon Doe" onChange={(e)=> setFullName(e.target.value)} value={fullName}
+				></input>
+
       <label> Email </label>
-				<input type="email" required placeholder="jhon@email.com"
+				<input type="email" required placeholder="jhon@email.com" onChange={(e)=> setEmail(e.target.value)} value={email}
 				></input>
 
 				<label> Hasło </label>
         <div className="password-container">
-				<input type="password" required placeholder="**********"
+				<input type={InputType} required placeholder="**********"  onChange={(e)=> setPassword(e.target.value)} value={password}
 				></input>
          <span className="password-toggle-icon">{Icon}</span>
         </div>
